00fa7568e62c86e7131099cffa1d06ca4ac7a55e
madortill <mador.till@gmail.com>
2024-11-18 16:07:44 +0200

    before im beginning

---
+++ src/components/MainScreen.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="main-screen">
+        
+    </div>
+</template>
+
+<script>
+export default {
+  name: "main-screen",
+  data() {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+#main-screen {
+    width: 100%;
+    height: 100%;
+    background-color: black;
+}
+</style>
