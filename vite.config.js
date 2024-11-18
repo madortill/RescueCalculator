72a248606343c099312c28167f47eb9ab9ce72b6
madortill <mador.till@gmail.com>
2024-11-18 16:20:08 +0200

    trying different vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   },
+//   base: process.env.NODE_ENV === "production" ? "/RescueCalculator/" : "/",
+// })
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -12,4 +26,15 @@ export default defineConfig({
     }
   },
   base: process.env.NODE_ENV === "production" ? "/RescueCalculator/" : "/",
+  build: {
+    // Control how the filenames are generated
+    rollupOptions: {
+      output: {
+        // Modify the entry file name
+        entryFileNames: 'index.js',
+        chunkFileNames: '[name].js',  // Custom naming for chunks
+        assetFileNames: '[name].[ext]' // Custom naming for assets
+      }
+    }
+  }
 })
