17af3c3b4660518dac535bc4bc76d0ebb7471703
madortill <mador.till@gmail.com>
2024-11-19 18:24:23 +0200

    plzzzzzzzzzzzzzzzz

--- src/components/MainScreen.vue
+++ src/components/MainScreen.vue
@@ -31,8 +31,8 @@ export default {
 #main-screen {
     width: 100%;
     height: 100%;
-    /* background-color: black; */
-    background-color: white;
+    background-color: black;
+    /* background-color: white; */
     background-size: 110% 110%;
 }
 
