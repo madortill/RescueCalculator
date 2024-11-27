ec9d8edb043f91c967963253153511d0d077d4ed
madortill <mador.till@gmail.com>
2024-11-27 12:37:40 +0200

    mekadem

--- src/components/Calculator2SVG.vue
+++ src/components/Calculator2SVG.vue
@@ -79,7 +79,7 @@
         <g class="button">
             <rect class="cls-6" x="82.42" y="197.43" width="69.41" height="151.42" rx="17.78" ry="17.78" transform="translate(-156.02 390.26) rotate(-90)" />
             <text class="cls-2" transform="translate(56.51 281.72) scale(1.06 1)">
-                <tspan x="120" y="0">סוג קרקע</tspan>
+                <tspan x="120" y="0">מקדם קרקע</tspan>
             </text>
         </g>
         <g class="button">
